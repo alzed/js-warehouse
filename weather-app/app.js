292ec3e8eca1ffbb5f78371c7e9b002acba31291
alzed <madhumithap.ravishankar@gmail.com>
2020-06-20 17:52:00 +0530

    Update app.js
    Add command line argument

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,19 +1,25 @@
 const geocode = require('./utils/geocode');
 const weather = require('./utils/weather');
 
-geocode('Chennai', (error, data) => {
-    if (error) {
-        console.log('Error:', error);
-    } else {
-        weather(data.latitude, data.longitude, (error, forecast) => {
-            if (error) {
-                console.log('Error:', error);
-            } else {
-                let location = `${forecast.location}, ${forecast.region}, ${forecast.country}`;
-                console.log(location);
-                console.log('Weather: ', forecast.weather);
-                console.log('Temperature: ', forecast.temperature);
-            }
-        });
-    }
-});
+const address = process.argv[2];
+
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            console.log('Error:', error);
+        } else {
+            weather(data.latitude, data.longitude, (error, forecast) => {
+                if (error) {
+                    console.log('Error:', error);
+                } else {
+                    let location = `${forecast.location}, ${forecast.region}, ${forecast.country}`;
+                    console.log(location);
+                    console.log('Weather: ', forecast.weather);
+                    console.log('Temperature: ', forecast.temperature);
+                }
+            });
+        }
+    });
+}
