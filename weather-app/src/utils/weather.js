119666dec34a48b1ad660203d0fab68310995707
alzed <madhumithap.ravishankar@gmail.com>
2020-07-13 22:18:35 +0530

    Update weather to use axios

--- weather-app/src/utils/weather.js
+++ weather-app/src/utils/weather.js
@@ -1,28 +1,32 @@
-const request = require('request');
+const axios = require('axios');
 const config = require('../config');
 
 const weatherApi = config.weather_api_key;
 
-const weather = (latitude, longitude, callback) => {
+const weather = (latitude, longitude) => {
     const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherApi}&query=${latitude},${longitude}`;
 
-    request({url: weatherUrl, json:true},
-        (error, response) => {
-            if (error) {
-                callback('Unable to connect to weather services', undefined);
-            } else if (response.body.error) {
-                callback('Unable to find location', undefined);
-            } else {
-                callback(undefined, {
-                    location: response.body.location.name, 
-                    region: response.body.location.region,
-                    country: response.body.location.country,
-                    weather: response.body.current.weather_descriptions[0],
-                    temperature: response.body.current.temperature   
-                });
-            }
+    const weatherData = axios({
+        method: 'get',
+        url: weatherUrl, 
+        responseType: 'json'
+    }).then(response => {
+        if (response.data.error) {
+            return ('Unable to find location', undefined);
+        } else {
+            return (undefined, {
+                location: response.data.location.name, 
+                region: response.data.location.region,
+                country: response.data.location.country,
+                weather: response.data.current.weather_descriptions[0],
+                temperature: response.data.current.temperature   
+            });
         }    
-    ); 
+    }).catch(error => {
+        return ('Unable to connect to weather services', undefined);
+    }); 
+
+    return weatherData;
 };
 
 module.exports = weather;
