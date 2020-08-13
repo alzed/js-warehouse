5685fa8a3e2b61a463825f9618517a66391dcf12
alzed <madhumithap.ravishankar@gmail.com>
2020-08-13 23:20:14 +0530

    Update app.js
    Add loading message

--- weather-app/static/js/app.js
+++ weather-app/static/js/app.js
@@ -7,13 +7,13 @@ search.addEventListener('click', (e) => {
     e.preventDefault();
     const address = document.getElementById('address-bar').value;
     if (address) {
-        console.log(address);
+        addressDiv.innerHTML = '<center>Loading...</center>';
         fetch(`/weather?address=${address}`).then(response => {
             response.json().then(({location, region, country, weather, temperature}) => {
                 addressDiv.innerHTML = `${location}, ${region}, ${country}`;
                 forecastDiv.innerHTML = `${weather}, ${temperature}° C`;
             }).catch(error => {
-                addressDiv.innerHTML = error.error;
+                addressDiv.innerHTML = error;
             });
         })
     } else {
